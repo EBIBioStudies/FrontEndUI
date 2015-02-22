f2c91df5a19a1e0a7e08a2fb9e826152af8b1001
arrayexpress <arrayexpress@ebi.ac.uk>
2015-02-22 19:07:41 +0000

    Final update of more/less button style

--- webapp/src/main/webapp/assets/scripts/jquery.common-1.0.150220.js
+++ webapp/src/main/webapp/assets/scripts/jquery.common-1.0.150220.js
@@ -420,13 +420,13 @@
 
         $data.find(".hidden-values").each(function () {
             var span = $(this);
-            var showMore = $("<a class=show-more href=#>show " + span.attr("size") + " more &gt;</a>")
+            var showMore = $("<a class=show-more href=#>show " + span.attr("size") + " more</a>")
                 .click(function (e) {
                     e.preventDefault();
                     e.stopPropagation();
                     $(this).hide().next().show();
                 });
-            var showLess = $("<a class=show-less href=#>&lt; show less</a>")
+            var showLess = $("<a class=show-less href=#>show less</a>")
                 .click(function (e) {
                     e.preventDefault();
                     e.stopPropagation();
